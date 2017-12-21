64254965a22a6d3264f6d522a5282ed73a031379
Patrick Perry <patperry@gmail.com>
2017-12-21 10:57:54 -0500

    add 'subset2'

--- R/index.R
+++ R/index.R
@@ -12,6 +12,12 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
+`[[.dataset` <- function(x, i, exact = TRUE)
+{
+    # ignore the 'exact' argument
+    # no fancy subscripting here; maybe add support for that later
+    .subset2(x, i)
+}
 
 `[[<-.dataset` <- function(x, i, value)
 {
@@ -46,7 +52,7 @@
 
 `$.dataset` <- function(x, name)
 {
-    x[[name, exact = TRUE]]
+    x[[name]]
 }
 
 
